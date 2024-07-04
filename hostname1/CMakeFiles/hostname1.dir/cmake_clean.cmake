329c33ba9cf2ac8f125d8a3c72abb5d9265ef636
Salah Abotaleb <salah.m.abotaleb@gmail.com>
2024-07-04 18:48:28 +0300

    QT generated files

---
+++ hostname1/CMakeFiles/hostname1.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hostname1.dir/hostname1.cpp.o"
+  "CMakeFiles/hostname1.dir/hostname1.cpp.o.d"
+  "CMakeFiles/hostname1.dir/main.cpp.o"
+  "CMakeFiles/hostname1.dir/main.cpp.o.d"
+  "hostname1"
+  "hostname1.cpp"
+  "hostname1.h"
+  "hostname1.moc"
+  "hostname1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hostname1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
