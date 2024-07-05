54958cb249ed86f37fcf73aebdb1ac6af5ded8e3
salah <salah.m.abotaleb@gmail.com>
2024-07-05 12:02:11 +0300

    Edit main file and CMAKELISTS to register the service in session instead of system bus

--- hostname1/main.cpp
+++ hostname1/main.cpp
@@ -1,6 +1,26 @@
+#include <sysexits.h>
+
+#include <QCoreApplication>
+#include <QtDBus>
 #include <iostream>
-using namespace std;
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+
+#include "hostname1.h"
+#include "hostname1adaptor.h"
+int main(int argc, char *argv[]) {
+    QCoreApplication app(argc, argv);
+    QDBusConnection sessionBus = QDBusConnection::sessionBus();
+    if (!sessionBus.registerService("org.freedesktop.hostname1")) {
+        qCritical() << "Could not register hostname1 service";
+        return EX_UNAVAILABLE;
+    }
+    Hostname1 hostname1;
+    new Hostname1Adaptor(&hostname1);
+    if (!sessionBus.registerObject("/org/freedesktop/hostname1", &hostname1)) {
+        qCritical() << "Could not register hostname1 object";
+        sessionBus.unregisterService("org.freedesktop.hostname1");
+        return EX_UNAVAILABLE;
+    }
+    std::cout << "Hostname1 service running" << std::endl;
+
+    return app.exec();
 }
